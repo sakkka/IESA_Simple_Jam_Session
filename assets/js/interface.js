d7ddb8d0d83f4e598d5d05e16bba7e7754598d12
Mastère 1 IT <mastere1it@FRO-206-22.fro-206.froment.in.iesanetwork.com>
2016-02-24 13:09:11 +0100

    users list

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -157,3 +157,12 @@ function generateLink(nameRoom){
 
 }
 
+function printConnectedUsers(data) {
+	$('.user-in-list').remove();
+	for (var i=0; i<data.length; i++) {
+		$(".users-list").append('<span class="user-in-list">'+data[i][1]+'</span>');
+	}
+}
+
+
+
